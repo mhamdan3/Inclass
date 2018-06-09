e80e6ffd90ba82c98221462360e3fc0af9379bb6
Mahdi Hamdan <hamdani0430@gmail.com>
2018-06-09 11:19:11 -0400

    .

--- Module_3 Server Side/Week_18/3_Saturday/Unsolved/userModel.js
+++ Module_3 Server Side/Week_18/3_Saturday/Unsolved/userModel.js
@@ -33,7 +33,7 @@ var UserSchema = new Schema({
   /* TODO:
    * Add four entries into our schema. These should be:
    *
-
+  
    * 1: username: A string that will be be required, and also trimmed.
    * 2: password: A string that will be required, trimmed, and at least 6 characters.
    * 3: email: A string that must be a valid email address and unique in our collection.
